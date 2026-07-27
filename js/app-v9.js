0296580f46499b35cc2d057db8fd5842266b4be3
pffngg <marrie4family@gmail.com>
2026-07-28 00:24:49 +0300

    Update app-v9.js

--- js/app-v9.js
+++ js/app-v9.js
@@ -1934,10 +1934,10 @@ function downloadFboCsv(shiftData) {
   let csv = '';
   articles.forEach(item => {
     const art = item.baseArticle || '';
-    const suffix = item.suffix || '';    // размер (S/F) или пусто для арок
+    const suffix = item.suffix || '';
     const material = item.material || '';
-    const width = item.width || '';      // толщина (1 ММ / 1,5 ММ) или пусто для арок
-    csv += `${art}\t${suffix}\t${material}\t${width}\n`;
+    const width = item.width || '';
+    csv += `${art};${suffix};${material};${width}\n`;
   });
 
   const blob = new Blob(['\uFEFF' + csv], { type: 'text/csv;charset=utf-8;' });
